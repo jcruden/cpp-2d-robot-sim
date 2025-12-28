6b01993ba706bf8d9521d8e88810be97c5b1ae28
jcruden <jennifer.cruden@gmail.com>
2025-12-28 12:08:24 -0800

    add obstacles and svg

--- src/sim/occupancy_grid.cpp
+++ src/sim/occupancy_grid.cpp
@@ -1,10 +1,12 @@
 #include "occupancy_grid.hpp"
 #include <cassert>
+#include <fstream>
 
-OccupancyGrid::OccupancyGrid(int width, int height, double resolution)
+OccupancyGrid::OccupancyGrid(int width, int height, double resolution, Vec2 origin)
     : width_(width),
     height_(height),
     resolution_(resolution),
+    origin_(origin),
     data_(width * height, 0)
     {
         assert(width_ > 0 && height_ > 0);
@@ -28,7 +30,55 @@ void OccupancyGrid::setOccupied(int i, int j, bool occ) {
 }
 
 std::pair<int, int> OccupancyGrid::worldToGrid(Vec2 const& p) const {
-    int j = static_cast<int>(std::floor(p.x / resolution_));
-    int i = static_cast<int>(std::floor(p.y / resolution_));
+    int j = static_cast<int>(std::floor((p.x - origin_.x) / resolution_));
+    int i = static_cast<int>(std::floor((p.y - origin_.y) / resolution_));
     return {i, j};
+}
+
+Vec2 OccupancyGrid::gridToWorld(int i, int j) const {
+    return Vec2{
+        origin_.x + (j + 0.5) * resolution_,
+        origin_.y + (i + 0.5) * resolution_
+    };
+}
+
+void OccupancyGrid::addRectangle(Vec2 center, double w, double h) {
+    for (int i = 0; i < height_; i++) {
+        for (int j = 0; j < width_; j++) {
+            Vec2 cell = gridToWorld(i, j);
+            if (cell.x < center.x + w / 2 && cell.x > center.x - w / 2
+            && cell.y < center.y + h / 2 && cell.y > center.y - h / 2) {
+                setOccupied(i, j, true);
+            }
+        }
+    }
+}
+void OccupancyGrid::addCircle(Vec2 center, double radius) {
+    for (int i = 0; i < height_; i++) {
+        for (int j = 0; j < width_; j++) {
+            if (center.distance(gridToWorld(i, j)) < radius){
+                setOccupied(i, j, true);
+            }
+        }
+    }
+}
+
+void OccupancyGrid::writeSVG(char const* filename) const {
+    std::ofstream out(filename);
+    out << "<svg xmlns=\"http://www.w3.org/2000/svg\" "
+        << "width=\"" << width_ * 10 << "\" "
+        << "height=\"" << height_ * 10 << "\">\n";
+
+    for (int i = 0; i < height_; i++) {
+        for (int j = 0; j < width_; j++) {
+            const std::string fill = isOccupied(i, j) ? "black" : "white";
+
+            out << "<rect x=\"" << j * 10
+                << "\" y=\"" << i * 10
+                << "\" width=\"10\" height=\"10\" "
+                << "fill=\"" << fill << "\" />\n";
+        }
+    }
+
+    out << "</svg>\n";
 }
--- src/sim/occupancy_grid.hpp
+++ src/sim/occupancy_grid.hpp
@@ -6,7 +6,7 @@
 
 class OccupancyGrid {
 public:
-    OccupancyGrid(int width, int height, double resolution);
+    OccupancyGrid(int width, int height, double resolution, Vec2 origin = {0.0, 0.0});
 
     bool isOccupied(int i, int j) const;
     void setOccupied(int i, int j, bool occ = true);
@@ -14,13 +14,23 @@ public:
     int width() const noexcept { return width_; };
     int height() const noexcept { return height_; };
     
+    // conversions
     std::pair<int, int> worldToGrid(Vec2 const& p) const;
+    Vec2 gridToWorld(int i, int j) const;
+
+    // obstacles
+    void addRectangle(Vec2 center, double w, double h);
+    void addCircle(Vec2 center, double radius);
+
+    void writeSVG(char const* filename) const;
+    
+private:
     int index(int i, int j) const noexcept;
     std::pair<int, int> coord(int ind) const;
 
-private:
     int width_;
     int height_;
     double resolution_;
+    Vec2 origin_;
     std::vector<uint8_t> data_; // 0 = free, 1 = occupied
 };
